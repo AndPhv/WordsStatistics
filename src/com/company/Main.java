19bb9dd4d04b6adb23e539be327e6fdbb0e95c4d
AndPhv <puhov2001@gmail.com>
2022-04-16 13:05:18 +0300

    Создаем наш текст и разбиваем его на массив слов

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    String text = "Hello hello java Java world";
+        String[] words = text.split(" ");
     }
 }
