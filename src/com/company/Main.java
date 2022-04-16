170b89405bef52f6c8ed28a5595f752d3c5d99c4
AndPhv <puhov2001@gmail.com>
2022-04-16 13:19:53 +0300

    Создание Map, где в качестве ключа будет слово, а в качестве значения - количество слов

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	    String text = "Hello hello java Java world";
         String[] words = text.split(" ");
+        System.out.println(getStatisticMap(words));
+    }
+    private static Map<String, Integer> getStatisticMap(String[] words) {
+
     }
 }
